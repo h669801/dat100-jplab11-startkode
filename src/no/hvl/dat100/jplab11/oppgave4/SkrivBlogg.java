f0b8bf04c66ea63d39181233cb3d97ade32d9b86
Thomas <Thomas@80.202.237.157>
2022-11-13 23:36:30 +0100

    --

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -9,7 +9,18 @@ import no.hvl.dat100.jplab11.oppgave3.*;
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+		
+		String fil = filnavn;
+		String mappen = mappe;
+		PrintWriter skriver = new PrintWriter (fil);
+		
+		String sam = samling.toString();
+		
+		skriver.println(sam);
+		
+		skriver.close();
+		
 
-		throw new UnsupportedOperationException(TODO.method());
+//		throw new UnsupportedOperationException(TODO.method());
 	}
 }
